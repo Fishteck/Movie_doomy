41dae56e24e2708ec40bd0c62f531966b69e4b30
Roman Makukha <roman.makuha@gmail.com>
2021-01-28 15:57:39 +0300

    Initial commit

---
+++ app/src/main/java/android/example/moviedoomy/presentation/view/fragments/NewMoviesFragment.java
@@ -0,0 +1,24 @@
+package android.example.moviedoomy.presentation.view.fragments;
+
+import android.example.moviedoomy.R;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NewMoviesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_movies, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
